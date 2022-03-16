2dc9f1413bb48fc760617c8cf8e7cfe159d76977
honeypotfly <honeypotfly@protonmail.com>
2022-03-16 14:51:21 -0400

    cleanup and add alice to our api

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.json([{
-
-    name: "Bob",
-    email: "bob@example.com"
-}]))
+app.get('/', (req, res) => res.json([
+    {
+        name: "Bob",
+        email: "bob@example.com"
+    },
+    {
+        name: "Alice",
+        email: "alice@example.com"
+    }
+]))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
